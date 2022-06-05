ae9c4dbed8ed86b0b6bf01bf7e512b16694ab819
shakiba-charkhkar <ch.shakiba73@gmail.com>
2022-06-05 14:07:36 +0430

    ReadDataFromUrl

--- WebScraper/webscraper.go
+++ WebScraper/webscraper.go
@@ -2,9 +2,35 @@ package webscraper
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func ReadWebData() {
 	fmt.Println("Read web Data")
+	var URL string = "https://www.dan.me.uk/torlist"
+	response, err := http.Get(URL) //use package "net/http"
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	// Copy data from the response to standard output
+	// n, err1 := io.Copy(os.Stdout, response.Body) //use package "io" and "os"
+	// if err != nil {
+	// 	fmt.Println(err1)
+	// 	return
+	// }
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+	//result := strings.Split(bodyString, "\n")
+	fmt.Println(bodyString)
 
 }
