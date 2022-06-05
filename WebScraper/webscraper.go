b48dc923daadadd8a2aa2ec70bc555594dbc8f99
shakiba-charkhkar <ch.shakiba73@gmail.com>
2022-06-05 20:16:49 +0430

    UseElasticSearch

--- WebScraper/webscraper.go
+++ WebScraper/webscraper.go
@@ -1,11 +1,24 @@
 package webscraper
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
+
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	esapi "github.com/elastic/go-elasticsearch/v8/esapi"
 )
+
+type WebData struct {
+	IP  string `json:"ip"`
+	URL string `json:"url"`
+}
 
 func ReadWebData() {
 	fmt.Println("Read web Data")
@@ -30,7 +43,96 @@ func ReadWebData() {
 		log.Fatal(err)
 	}
 	bodyString := string(bodyBytes)
-	//result := strings.Split(bodyString, "\n")
-	fmt.Println(bodyString)
+	result := strings.Split(bodyString, "\n")
+	//fmt.Println(bodyString)
+	StorageData(result, URL)
+
+}
+func StorageData(dataArr []string, url string) {
+	log.SetFlags(0)
+
+	var (
+		r  map[string]interface{}
+		wg sync.WaitGroup
+	)
+
+	// Initialize a client with the default settings.
+	//
+	// An `ELASTICSEARCH_URL` environment variable will be used when exported.
+	//
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	// 1. Get cluster info
+	//
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+	// Check response status
+	if res.IsError() {
+		log.Fatalf("Error: %s", res.String())
+	}
+	// Deserialize the response into a map.
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+	// Print client and server version numbers.
+	log.Printf("Client: %s", elasticsearch.Version)
+	log.Printf("Server: %s", r["version"].(map[string]interface{})["number"])
+	log.Println(strings.Repeat("~", 37))
+
+	// 2. Index documents concurrently
+	//
+	for i, title := range dataArr {
+		wg.Add(1)
+
+		go func(i int, title string) {
+			defer wg.Done()
+
+			// Build the request body.
+			webIp := &WebData{
+				IP:  title,
+				URL: url}
+			data, err := json.Marshal(webIp)
+			if err != nil {
+				log.Fatalf("Error marshaling document: %s", err)
+			}
+
+			// Set up the request object.
+			req := esapi.IndexRequest{
+				Index: "test5",
+				//DocumentID: strconv.Itoa(i + 1),
+				Body:    bytes.NewReader(data),
+				Refresh: "true",
+			}
+
+			// Perform the request with the client.
+			res, err := req.Do(context.Background(), es)
+			if err != nil {
+				log.Fatalf("Error getting response: %s", err)
+			}
+			defer res.Body.Close()
+
+			if res.IsError() {
+				log.Printf("[%s] Error indexing document ID=%d", res.Status(), i+1)
+			} else {
+				// Deserialize the response into a map.
+				var r map[string]interface{}
+				if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+					log.Printf("Error parsing the response body: %s", err)
+				} else {
+					// Print the response status and indexed document version.
+					log.Printf("[%s] %s; version=%d", res.Status(), r["result"], int(r["_version"].(float64)))
+				}
+			}
+		}(i, title)
+	}
+	wg.Wait()
+
+	log.Println(strings.Repeat("=", 37))
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module APKElasticSearch
 
 go 1.18
+
+require github.com/elastic/go-elasticsearch/v8 v8.2.0
+
+require github.com/elastic/elastic-transport-go/v8 v8.1.0 // indirect
